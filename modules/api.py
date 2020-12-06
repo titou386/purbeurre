b73e33510850402d833c47903d90e58a27c86b10
Yves <yves.dornier@yahoo.fr>
2020-12-06 22:27:04 +0100

    Corrections in api, database and requirements.txt

--- modules/api.py
+++ modules/api.py
@@ -1,41 +1,34 @@
 import requests
 import json
+import logging
 
 
-class OpenFactFood:
+
+
+class OpenFoodFacts:
 
 
     def __init__(self, nb_cat, nb_prod):
 
         self.nb_cat = nb_cat
-        self.top_cat = []
+        self.categories = []
         self.nb_prod = nb_prod
         self.products = []
-        self.err = None
-        self.get_top_categories()
-        self.get_products()
+        self.get_categoriesegories()
 
-    def log(self, msg):
-        print(msg)
 
-    def get_top_categories(self):
-        self.top_cat = [None for e in range(self.nb_cat)]
+    def get_categoriesegories(self):
+        self.categories = [None for e in range(self.nb_cat)]
 
-        r = requests.get('https://fr.openfoodfacts.org/categories.json')
-        if not r.ok:
-            self.log('Une erreur s\'est produite lors de la récupération des catégories')
-            self.log('Code erreur HTTP : ' + r.status_code)
-            self.err = r.status_code
-            return
+        r = requester('https://fr.openfoodfacts.org/categories.json')
 
-        data = json.loads(r.text)
-        for category in data['tags']:
-            for i in range(len(self.top_cat)):
-                if self.top_cat[i] is None or \
-                   self.top_cat[i]["products"] < category["products"]:
+        for category in r['tags']:
+            for i in range(len(self.categories)):
+                if self.categories[i] is None or \
+                   self.categories[i]["products"] < category["products"]:
 
-                    self.top_cat.insert(i, category)
-                    self.top_cat.pop(len(self.top_cat) - 1)
+                    self.categories.insert(i, category)
+                    self.categories.pop(len(self.categories) - 1)
                     break
 
 
@@ -43,12 +36,12 @@ class OpenFactFood:
         if self.err is not None:
             return
 
-        for j in range(len(self.top_cat)):
+        for cat in self.categories:
             payload = {
                 'action': 'process',
                 'tagtype_0': 'categories',
                 'tag_contains_0': 'contains',
-                'tag_0': self.top_cat[j]['id'],
+                'tag_0': self.cat['id'],
                 'tagtype_1': 'countries',
                 'tag_contains_1': 'contains',
                 'tag_1': 'France',
@@ -56,14 +49,20 @@ class OpenFactFood:
                 'json': 'true'
             }
 
-            r = requests.get('https://world.openfoodfacts.org/cgi/search.pl', params=payload)
-            if not r.ok:
-                self.log('Une erreur s\'est produite lors de la récupération des produits')
-                self.log('Code erreur HTTP : ' + r.status_code)
-                self.err = r.status_code
-                return
+            r = requester('https://world.openfoodfacts.org/cgi/search.pl', params=payload)
 
-            self.products.append(json.loads(r.text)['products'])
+            self.products.append(r['products'])
 
 
+def requester(self, url, **kwargs):
+    if ('params' in kwargs):
+        r = requests.get(url, kwargs['params'])
+    else:
+        r = requests.get(url)
 
+    if r.ok:
+        return json.loads(r.text)
+    else:
+        logging.error('Une erreur s\'est produite lors de la récupération des données')
+        logging.error('Code erreur HTTP : ' + r.status_code)
+        return None
--- modules/database.py
+++ modules/database.py
@@ -1,117 +1,104 @@
 import mysql.connector
 from mysql.connector import errorcode
+import logging
 
 
 class Mysql:
-    def __init__(self, host_db, user_db, password_db, database_db):
-        self.db_type = db_type
-        self.error_encountered = False
-        self.error_msgs = []
+    def __init__(self, db_host, db_user, db_password, db_name):
 
         try:
             cnx = mysql.connector.connect(
-                host=host_db,
-                user=user_db,
-                password=password_db,
-                database=database_db
+                host=db_host,
+                user=db_user,
+                password=db_password,
+                database=db_name
             )
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                print("Something is wrong with your user name or password")
+                logging.error("Something is wrong with your user name or password")
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
-                print("Database does not exist")
+                logging.error("Database does not exist")
             else:
-                print(err)
+                logging.error("Something went wrong at the connection :")
+                logging.error(err)
             exit(1)
 
         self.cursor = cnx.cursor()
 
-    def error(self, msg):
-        if msg == 'clear':
-            self.error_msgs = []
-            self.error_encountered = False
-        else:
-            self.error_encountered = True
-            self.error_msgs.append(msg)
-
     def execute(self, sql, values):
         try:
-            self.cursor.execute(sql, values)
+            if not values or len(values) == 1:
+                self.cursor.execute(sql, values)
+            else:
+                self.cursor.executemany(sql, values)
+
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-                self.error("Table already exists.")
+                logging.error("Table already exists.")
             elif err.errno == errorcode.ER_NO_SUCH_TABLE_ERROR:
-                self.error("Table doesn't exist")
+                logging.error("Table doesn't exist")
             elif err.errno == errorcode.ER_WRONG_COLUMN_NAME_ERROR:
-                self.error("Column doesn't exist")
-            self.error(err.msg)
-            self.error(sql.format(values))
+                logging.error("Column doesn't exist")
+            else:
+                logging.error(err.msg)
+            logging.error(sql.format(values))
+            return 1
         else:
-            print("OK")
+            logging.info("Succeeded")
+            logging.info(sql.format(values))
+            return 0
 
     def create_table(self, name, *col, **options):
-        values = ()
 
-        sql = "CREATE TABLE IF NOT EXIST {} ("
-        values.append(name)
-        for i, x in enumerate(col):
-            if i != 0:
-                sql += ', '
-            sql += '{}'
-            values.append(x)
-        sql += ')'
+        sql = "CREATE TABLE {} (".format(name)
+        sql += (', '.join(col))
+        sql += (')')
 
         for arg in options:
-            sql += ' {}={}'
-            values.append(arg.upper())
-            values.append(options[arg].upper())
+            sql += (' {}={}'.format(arg.upper(), options[arg].upper()))
 
-        self.execute(sql, values)
+        return self.execute(sql)
 
-    def insert(self, table, values_order, *values):  # values_order --> list, values --> scale
-        values = ()
+    def insert(self, table, values_names, values):  # values_names --> list, values --> list of dicts
 
-        sql = "INSERT INTO {} ("
-        values.append(table)
-        for i, val_name in enumerate(values_order):
-            if i != 0:
-                sql += ', '
-            sql += '{}'
-            values.append(val_name)
-        sql += ") VALUES "
+        sql = "INSERT INTO {} ({}) VALUES ({})"
 
-        for j, line_vals in enumerate(values):
-            if j != 0:
-                sql += ', '
-            for i, val in enumerate(line_vals):
-                if i == 0:
-                    sql += '('
-                if i != 0:
-                    sql += ', '
-                sql += '\'{}\''
-                values.append(val)
-            sql += ')'
+        data_quantity = ', '.join(['%s' for e in range(len(values_names))])
+        data = [(tuple([val[name]
+                for name in values_names])) for val in values]
 
-        self.execute(sql, values)
+        sql = sql.format(table, ', '.join(values_names), data_quantity)
 
-    def query(self, table, select='*', *where, **options):
-        values = ()
+        return self.execute(sql, data)
 
-        sql = "SELECT {} FROM {}"
-        values.append(select)
-        values.append(table)
-        for i, arg in enumerate(where):
-            if i == 0:
-                sql += " WHERE"
-            sql += ' {}'
-            values.append(arg)
 
-        for key in options:
-            sql += ' {} {}'
-            values.append(key.upper())
-            values.append(options[key])
+    def query(self, table, *values, select='*', **options):
+        if 'distinct' in options:
+            sql = "SELECT DISTINCT {} FROM {}".format(select, table)
+        else:
+            sql = "SELECT {} FROM {}".format(select, table)
 
-        self.execute(sql, values)
+        if 'where' in options:
+            sql += " WHERE {}".format(options['where'])
 
-    def update(self, table_name, set, where):
+        if 'in' in options:
+            data_quantity = ', '.join(['%s' for e in range(len(values))])
+            sql += " IN {}".format(data_quantity)
+
+        if 'order_by' in options:
+            if 'direction' not in options:
+                options['direction'] = "ASC"
+            sql += " ORDER BY {} {}".format(options['order_by'], options['direction'].upper())
+
+        if 'limit' in options:
+            sql += " LIMIT {}".format(options['limit'])
+            if 'offset' in options:
+                sql += " OFFSET {}".format(options['offset'])
+
+        return self.execute(sql, values)
+
+    def update(self, table, set, where):
+        pass
+
+    def delete(self, table, where):
         pass
