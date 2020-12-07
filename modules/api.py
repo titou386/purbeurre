19f1822f52629631aea2f52f958c94c9dc91baa2
Yves <yves.dornier@yahoo.fr>
2020-12-07 11:40:07 +0100

    Correction some mistakes and improvement result get_products in OpenFoodFacts

--- modules/api.py
+++ modules/api.py
@@ -1,23 +1,34 @@
+# -*- coding: utf-8 -*-
+"""api.py."""
 import requests
 import json
 import logging
 
 
-
-
 class OpenFoodFacts:
-
+    """Retrieves data from the Open Food Facts API."""
 
     def __init__(self, nb_cat, nb_prod):
+        """Open Food Facts' contructor require 2 arguments.
 
+        :param int nb_cat:
+            Defined how many categories should be retrieved
+
+        :param int nb_prod:
+            Defined how many products in each category should be retrieved
+        """
         self.nb_cat = nb_cat
         self.categories = []
         self.nb_prod = nb_prod
         self.products = []
-        self.get_categoriesegories()
+        self.get_categories()
 
+    def get_categories(self):
+        """Method get_categories.
 
-    def get_categoriesegories(self):
+        Retrieve from the API and fill 'categories' variable with
+        a decreasing ordered list.
+        """
         self.categories = [None for e in range(self.nb_cat)]
 
         r = requester('https://fr.openfoodfacts.org/categories.json')
@@ -31,17 +42,17 @@ class OpenFoodFacts:
                     self.categories.pop(len(self.categories) - 1)
                     break
 
-
     def get_products(self):
-        if self.err is not None:
-            return
+        """Method get_products.
 
+        Retrieve from the API and fill 'products' variable with a list.
+        """
         for cat in self.categories:
             payload = {
                 'action': 'process',
                 'tagtype_0': 'categories',
                 'tag_contains_0': 'contains',
-                'tag_0': self.cat['id'],
+                'tag_0': cat['id'],
                 'tagtype_1': 'countries',
                 'tag_contains_1': 'contains',
                 'tag_1': 'France',
@@ -49,12 +60,15 @@ class OpenFoodFacts:
                 'json': 'true'
             }
 
-            r = requester('https://world.openfoodfacts.org/cgi/search.pl', params=payload)
+            r = requester('https://world.openfoodfacts.org/cgi/search.pl',
+                          params=payload)
 
-            self.products.append(r['products'])
+            self.products.append([prod for cat in r['products']
+                                  for prod in cat])
 
 
-def requester(self, url, **kwargs):
+def requester(url, **kwargs):
+    """Similar to requests.get."""
     if ('params' in kwargs):
         r = requests.get(url, kwargs['params'])
     else:
@@ -63,6 +77,7 @@ def requester(self, url, **kwargs):
     if r.ok:
         return json.loads(r.text)
     else:
-        logging.error('Une erreur s\'est produite lors de la récupération des données')
+        logging.error('Une erreur s\'est produite \
+                      lors de la récupération des données')
         logging.error('Code erreur HTTP : ' + r.status_code)
         return None
--- modules/database.py
+++ modules/database.py
@@ -1,11 +1,28 @@
+# -*- coding: utf-8 -*-
+"""database.py."""
 import mysql.connector
 from mysql.connector import errorcode
 import logging
 
 
 class Mysql:
-    def __init__(self, db_host, db_user, db_password, db_name):
+    """Mysql interface for purbeurre application."""
 
+    def __init__(self, db_host, db_user, db_password, db_name):
+        """Connection to Mysql.
+
+        :param str db_host:
+            Mysql server host.
+
+        :param str db_user:
+            Username for access to the database(db_name).
+
+        :param str db_password:
+            Password of the username.
+
+        :param str db_name:
+            Database name.
+        """
         try:
             cnx = mysql.connector.connect(
                 host=db_host,
@@ -15,7 +32,8 @@ class Mysql:
             )
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-                logging.error("Something is wrong with your user name or password")
+                logging.error("Something is wrong \
+                    with your user name or password")
             elif err.errno == errorcode.ER_BAD_DB_ERROR:
                 logging.error("Database does not exist")
             else:
@@ -26,6 +44,7 @@ class Mysql:
         self.cursor = cnx.cursor()
 
     def execute(self, sql, values):
+        """Transmit sql request to Mysql server."""
         try:
             if not values or len(values) == 1:
                 self.cursor.execute(sql, values)
@@ -49,18 +68,37 @@ class Mysql:
             return 0
 
     def create_table(self, name, *col, **options):
+        """Create one table in the database.
 
-        sql = "CREATE TABLE {} (".format(name)
-        sql += (', '.join(col))
-        sql += (')')
+        :param str col:
+            Descritpion sql of the colonnes
+            ex: "id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT "
+
+        :param dict options
+            Option of the table
+            ex engine=innodb
+        """
+        col_sql_format = (', '.join(col))
+        sql = "CREATE TABLE {} ({})".format(name, col_sql_format)
 
         for arg in options:
             sql += (' {}={}'.format(arg.upper(), options[arg].upper()))
 
         return self.execute(sql)
 
-    def insert(self, table, values_names, values):  # values_names --> list, values --> list of dicts
 
+    def insert(self, table, values_names, values):
+        """Insert one or few lines of data into the table.
+
+        :param list values_names
+            List of colonnes names
+            (Must be the same keys values)
+
+        :param dict values
+            key: data
+            key = colonne name
+            data = data should be inserted
+        """
         sql = "INSERT INTO {} ({}) VALUES ({})"
 
         data_quantity = ', '.join(['%s' for e in range(len(values_names))])
@@ -71,8 +109,17 @@ class Mysql:
 
         return self.execute(sql, data)
 
-
     def query(self, table, *values, select='*', **options):
+        """Query to database.
+
+        :param str table:
+            Same as FROM in sql
+            Table name for query
+
+        :param list values
+
+        :param dict options
+        """
         if 'distinct' in options:
             sql = "SELECT DISTINCT {} FROM {}".format(select, table)
         else:
@@ -88,7 +135,8 @@ class Mysql:
         if 'order_by' in options:
             if 'direction' not in options:
                 options['direction'] = "ASC"
-            sql += " ORDER BY {} {}".format(options['order_by'], options['direction'].upper())
+            sql += " ORDER BY {} {}".format(options['order_by'],
+                                            options['direction'].upper())
 
         if 'limit' in options:
             sql += " LIMIT {}".format(options['limit'])
@@ -98,7 +146,9 @@ class Mysql:
         return self.execute(sql, values)
 
     def update(self, table, set, where):
+        """Not yet implemented."""
         pass
 
     def delete(self, table, where):
+        """Not yet implemented."""
         pass
