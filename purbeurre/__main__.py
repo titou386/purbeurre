abbe219abe70fb87dd61e1d58c2673996a854c6f
Yves <yves.dornier@yahoo.fr>
2021-01-16 17:35:02 +0100

    __main__.py

---
+++ purbeurre/__main__.py
@@ -0,0 +1,11 @@
+"""Main."""
+import purbeurre.controller
+
+
+def main():
+    """Main function."""
+    control = purbeurre.controller.Controller()
+    control.run()
+
+if __name__ == '__main__':
+    main()
